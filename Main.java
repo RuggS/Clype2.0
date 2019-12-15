0d0dfbe931ce482bb40e78ac723623f7662ebc65
chrisundercoffer <55003067+chrisundercoffer@users.noreply.github.com>
2019-12-14 19:00:06 -0500

    Add files via upload

--- Main.java
+++ Main.java
@@ -1,21 +1,40 @@
 package application;
 	
+import data.*;
+import main.*;
 import javafx.application.Application;
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
 import javafx.stage.Stage;
 import javafx.scene.Scene;
-import javafx.scene.layout.BorderPane;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
+import javafx.scene.layout.FlowPane;
 
 
 public class Main extends Application {
 	@Override
 	public void start(Stage primaryStage) {
 		try {
-			BorderPane root = new BorderPane();
+			FlowPane root = new FlowPane();
+			
+			TextField usrMsg = new TextField();
+			
+			Button sendButton = new Button("Send Message");
+			Button loadButton = new Button("Send Media");
+			
+			
+			
+			
+			root.getChildren().add(usrMsg);
+			root.getChildren().add(sendButton);
+			root.getChildren().add(loadButton);
+			
+
+			
 			Scene scene = new Scene(root,400,400);
 			scene.getStylesheets().add(getClass().getResource("application.css").toExternalForm());
-			
-			
-			
 			primaryStage.setScene(scene);
 			primaryStage.show();
 		} catch(Exception e) {
