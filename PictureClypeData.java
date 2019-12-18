88a36c57461a7d11f41958b36ff993ba92d37967
chrisundercoffer <55003067+chrisundercoffer@users.noreply.github.com>
2019-12-18 16:29:37 -0500

    Add files via upload

---
+++ PictureClypeData.java
@@ -0,0 +1,53 @@
+package data;
+
+import java.awt.image.BufferedImage;
+import java.io.ByteArrayInputStream;
+import java.io.ByteArrayOutputStream;
+import java.io.File;
+import java.io.IOException;
+import java.io.InputStream;
+
+import javax.imageio.ImageIO;
+
+public class PictureClypeData extends ClypeData {
+
+	private String imagePath = "";
+	private transient  BufferedImage img;
+	//private File imgFile;
+	private byte[] barr;
+	public PictureClypeData(String imgPath, String usern, int t) {
+		super(usern, t);
+		this.imagePath = imgPath;
+		try {
+			this.img = ImageIO.read(new File(imagePath));
+			ByteArrayOutputStream baos = new ByteArrayOutputStream();
+			ImageIO.write(img, "png", baos);
+			baos.flush();
+			barr = baos.toByteArray();
+			baos.close();
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
+
+	@Override
+	public Object getData() {
+		InputStream in = new ByteArrayInputStream(this.barr);
+		try {
+			img = ImageIO.read(in);
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		return imagePath;
+	}
+
+	@Override
+	public Object getData(String key) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+	
+	
+}
